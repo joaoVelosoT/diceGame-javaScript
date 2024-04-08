6b45c4c3bd094b3e3dd5a2bc063d4be82dc8d9ac
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2024-04-08 13:42:52 -0300

    adicionando comentarios

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ function switchPlayer() {
         playerActive1.classList.remove("player--active");
     }
 }
+
 // Criando uma function para adicionar o numSorteado na current do player ativo
 function adicionarCurrent(numSorteado) {
     if (playerActive0.classList.contains("player--active")) {
@@ -45,22 +46,20 @@ function adicionarCurrent(numSorteado) {
     }
 }
 
-
+// Pegando alguns elementos pelo id
 const scorePlayer0 = document.getElementById("score--0");
 const scorePlayer1 = document.getElementById("score--1");
-
 const currentPlayer0 = document.getElementById("current--0");
 const currentPlayer1 = document.getElementById("current--1");
 
-
+// Criando algumas variaveis para manipular o score dos players
 var score0 = 0;
 var score1 = 0;
-
 var current0 = 0;
 var current1 = 0;
 
+// Criando novo jogo
 btnNewGame.addEventListener('click', () => {
-    var vitoria = Math.floor(Math.random() * 50) + 1;
     current0 = 0;
     current1 = 0;
     score0 = 0;
@@ -74,7 +73,7 @@ btnNewGame.addEventListener('click', () => {
     playerGanhou.innerText = "";
 });
 
-
+// Rolando dado
 btnRoll.addEventListener('click', () => {
     var numSorteado = Math.floor(Math.random() * 6) + 1;
     console.log(numSorteado);
@@ -114,6 +113,7 @@ btnRoll.addEventListener('click', () => {
     }
 })
 
+// Adicionando o score
 function adicionarScore() {
     if (playerActive0.classList.contains("player--active")) {
         score0 += current0;
@@ -136,40 +136,35 @@ const name1 = document.getElementsByClassName("name--1")[0];
 const body = document.getElementsByClassName("body")[0];
 const playerGanhou = document.getElementById("ganhou");
 
+//Criando variaveis de audio
 var audio = new Audio('vitoria.mp3');
 var audioErro = new Audio('derrota.mp3');
 var audioHold = new Audio('holdAudio.mp3')
 
-function confettiFalling() {
 
+// Criando animação
+function confettiFalling() {
 	var box = document.getElementById("box");
 	var colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink'];
 
 	for (var i = 0; i < 1000; i++) {
-		
 		// Create 1000 DIV elements for confetti
 		var div = document.createElement("div");
 		div.classList.add("confetti");
 		box.appendChild(div);
 	}
-
 	var confetti = document.querySelectorAll('.confetti');
-
 	for (var i = 0; i < confetti.length; i++) {
-		
 		var size = Math.random() * 0.01 * [i];
-
 		confetti[i].style.width = 5 + size + 'px';
 		confetti[i].style.height = 16 + size + 'px';
 		confetti[i].style.left = Math.random() * innerWidth + 'px';
-
 		var background = colors[Math.floor(Math.random() * colors.length)];
 		confetti[i].style.backgroundColor = background;
-
 		box.children[i].style.transform = "rotate("+ size*[i] +"deg)";
 	}
 }
-
+// Salvando o score, e trocando de player
 btnHold.addEventListener('click', () =>{
     audioHold.play();
     adicionarScore();
@@ -196,6 +191,5 @@ btnHold.addEventListener('click', () =>{
         playerActive0.classList.add("player--active");
         playerActive1.classList.remove("player--active");
     }
-
 });
 
